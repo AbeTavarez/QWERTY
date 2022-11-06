584eb3d7d88ee2cb038160086c3fb2fb3a13a911
Abraham E. Tavarez <abetavarez@icloud.com>
2022-11-06 18:27:23 -0500

    component footer

--- components/Tweet/MainContainer/Footer/index.tsx
+++ components/Tweet/MainContainer/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { UserType, TweetType } from '../../../../types';
 import { styles } from './styles';
-import { Ionicons } from '@expo/vector-icons';
+import { Feather, EvilIcons, AntDesign } from '@expo/vector-icons';
 
 export type FooterProps = {
   tweet: TweetType;
@@ -10,7 +10,25 @@ export type FooterProps = {
 
 const Footer = ({ tweet }: FooterProps) => (
   <View style={styles.container}>
-    <Text>hello</Text>
+    {/* COMMENTS  */}
+    <View style={styles.iconContainer}>
+      <Feather name="message-circle" size={20} color="grey" />
+      <Text style={styles.number}>{tweet.numberOfComments}</Text>
+    </View>
+    {/* RETWEETS  */}
+    <View style={styles.iconContainer}>
+      <EvilIcons name="retweet" size={28} color="grey" />
+      <Text style={styles.number}>{tweet.numberOfRetweets}</Text>
+    </View>
+    {/* LIKES  */}
+    <View style={styles.iconContainer}>
+      <AntDesign name="hearto" size={20} color="grey" />
+      <Text style={styles.number}>{tweet.numberOfLikes}</Text>
+    </View>
+    {/* SHARES  */}
+    <View style={styles.iconContainer}>
+      <EvilIcons name="share-google" size={28} color="black" />
+    </View>
   </View>
 );
 export default Footer;
--- components/Tweet/MainContainer/Footer/styles.ts
+++ components/Tweet/MainContainer/Footer/styles.ts
@@ -2,7 +2,15 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-
+    flexDirection: 'row',
+    justifyContent: 'space-around'
   },
-
+  iconContainer: {
+    flexDirection: 'row',
+    alignContent: 'center'
+  },
+  number: {
+    color: 'grey',
+    marginLeft: 5
+  },
 })
